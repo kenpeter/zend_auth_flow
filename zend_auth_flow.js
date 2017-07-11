509e62dec0aeecacb6e45be01743184a89d3cdb4
kenpeter <figo2476@hotmail.com>
2017-07-11 11:10:57 +1000

    [fix] fix style

--- zend_auth_flow.js
+++ zend_auth_flow.js
@@ -116,68 +116,6 @@ app.get('/tickets/:id', async function(req, res) {
   }
 });
 
-/*
-app.get('/handle_user_decision', function(req, res) {
-  // we have code or error
-  if(req.query.code != undefined) {
-    const code = req.query.code;
-
-    // the access code is different each time
-    // access token can be the same in that amount of time.
-    console.log('-- access code --');
-    console.log(code);
-
-    const get_token_url = config.my_zend_root_url + "/oauth/tokens";
-
-    // Need to post and get token
-    const jsonDataObj = {
-      "grant_type": "authorization_code",
-      "code": code,
-      "client_id": config.client_id,
-      "client_secret": config.client_secret,
-      "redirect_uri": config.redirect_uri,
-      "scope": "read"
-    };
-
-    request.post({
-      url: get_token_url,
-      body: jsonDataObj,
-      json: true
-    }, function(error, response, body) {
-      // access token
-      // 65205bd62aca324120539a87de1de26fd4241da9518d24098bb26b2c277388d4
-      if (error) {
-        console.log('-- error --');
-        console.log(error);
-      }
-      else {
-        const access_token = body.access_token;
-
-        if(access_token) {
-          // remember token
-          res.cookie('access_token', access_token);
-          // res.send({set_success: true});
-
-          console.log('-- access token --');
-          console.log(access_token);
-
-          // rediect to listing ticket page, as we have access token
-          res.redirect('/');
-          console.log('-- redirect to home page --');
-        }
-        else {
-
-        }
-      }
-    });
-  }
-  else {
-    //
-    console.log('has no req.query.code');
-  }
-});
-*/
-
 // /handle_user_decision
 handleUserDecisionRoute(app);
 
